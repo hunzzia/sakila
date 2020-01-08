8172e6210b4a7bc8671c9320b60ce9bf021a4ccb
das6521 <dsa6521@naver.com>
2020-01-08 11:57:39 +0900

    index 페이지 내용 추가 chart-pie-js 데이터 값 수정 index controller 불필요한 메서드 삭제

--- sakila/WebContent/js/demo/chart-pie-demo.js
+++ sakila/WebContent/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Sports", "Sci-Fi ", "Animation " ,"Drama" , "Comedy"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [25,20,20,18,17],
+      backgroundColor: ['#4e73df', '#1cc88a', '#ffd633', "#ff5050" , "#4dd2ff"],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#ffcc00' , "#ff3333" ,"#1ac6ff"],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
